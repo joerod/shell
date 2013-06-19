46ced06bdb967a2dbf0a209a42942857743a9580
Joe Rodriguez <joerod@gmail.com>
2013-06-19 18:04:28 -0300

    Update GAM_Automation.sh

--- GAM_Automation.sh
+++ GAM_Automation.sh
@@ -3,7 +3,7 @@
 gam="python /gam/gam.py" #set this to the location of your GAM binaries
 DATE=`date +%Y-%m-%d`
 newuser(){
-   echo "        gApps Admin"
+   echo "    Patch gApps Admin"
    read -p "Enter email address to admin: " email
    }
 clear
@@ -19,10 +19,12 @@ do
  echo "4. Remove From All Groups"
  echo "5. Check Group Membership"
  echo "6. Perform All Tasks"
- echo "7. Mirror $email's Groups to another user"
- echo "8. Admin Another User"
- echo "9. Exit"
- echo "Please enter option [1 - 9]"
+ echo "7. Show all calendars"
+ echo "8. Mirror $email's Groups to another user"
+ echo "9. Suspend User"
+ echo "10. Admin Another User"
+ echo "11. Exit"
+ echo "Please enter option [1 - 11]"
     read opt
     case $opt in
      1) echo "************ Set Vacation Message / Remove Forward *************";
@@ -41,7 +43,7 @@ do
         echo "Press [enter] key to continue. . .";
         read enterKey;;
      4) echo "************ Remove From All Groups ************";
-        purge_groups=$($gam info user $email | grep -A 100 "Groups:" |cut -d '<' -f2 |cut -d '>' -f1 |grep -v 'Groups:')
+        purge_groups=$(grep -A 100 "Groups:" |cut -d '<' -f2 |cut -d '>' -f1 |grep -v 'Groups:')
            for i in $purge_groups
             do
                echo removing $i |$gam update group $i remove member $email
@@ -66,25 +68,34 @@ do
             done;
         echo "All tasks preformed press [enter] key to continue. . .";
         read enterKey;;
-      
-     7) echo "************ Mirror $email groups to another user ************";
+     7) echo "************ Show all calendars ************";
+        $gam user $email show calendars
+         echo "All tasks preformed press [enter] key to continue. . .";
+        read enterKey;;
+
+     8) echo "************ Mirror $email groups to another user ************";
         read -p  "Enter email address to be mirrored: " mirrored;
         echo $email groups will be mirrored to $mirrored press enter if this is OK?;
         read enterKey;
-         purge_groups=$($gam info user $email | grep -A 100 "Groups:" |cut -d '<' -f2 |cut -d '>' -f1 |grep -v 'Groups:')
+         purge_groups=$(grep -A 100 "Groups:" |cut -d '<' -f2 |cut -d '>' -f1 |grep -v 'Groups:')
            for i in $purge_groups
             do
                echo adding $mirror to $i group  |$gam update group $i add member $mirrored
             done;
         echo "All groups have been mirrored press [enter] key to continue. . .";
         read enterKey;;
-     8) echo "************ Admin Another User ************";
+     9) echo "************ Suspend  User ************";
+        $gam update user $email suspended on
+        echo "User is now suspended press [enter] key to continue. . .";
+        read enterKey;;
+
+     10) echo "************ Admin Another User ************";
         newuser;       
         echo "Press [enter] key to continue. . .";
         read enterKey;;
-     9) echo "Bye $USER";
+     11) echo "Bye $USER";
         exit 1;; 
-     *) echo "$opt is an invaild option. Please select option between 1-9 only"
+     *) echo "$opt is an invaild option. Please select option between 1-10 only"
        echo "Press [enter] key to continue. . .";
         read enterKey;;
 esac
