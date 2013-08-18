d3397cb26b51d7949303cb8ace1fa059cd89e566
Joe Rodriguez <joerod@gmail.com>
2013-08-18 17:59:26 -0400

    Update bulk_group_remove.sh

--- bulk_group_remove.sh
+++ bulk_group_remove.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-#this will bulk remove users from all groups in Google apps, I uses GAM to execute the script.
+#this will bulk remove users from all groups in Google apps, I use GAM to execute the script.
 
 gam="python $HOME/Documents/gam/gam.py" #set this to the location of your GAM binaries
 while read -r line
 do
- purge_groups=$($gam info user $line | grep -A 100 "Groups:" |cut -d '<' -f2 |cut -d '>' -f1 |sort )
+
+ purge_groups=$($gam info user $email | grep -A 100 "Groups:" |cut -d '<' -f2 |cut -d '>' -f1 |grep -v 'Groups:')
+
 for i in $purge_groups
             do
                echo removing $line from $i
