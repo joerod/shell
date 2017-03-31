000b5748925259e882a2beacfcabbae83b5b6ba1
Rob Olague <robolague@users.noreply.github.com>
2017-03-31 09:53:23 -0700

    Update GAM_Automation.sh

--- GAM_Automation.sh
+++ GAM_Automation.sh
@@ -55,7 +55,7 @@ do
         read enterKey;;
     
      4) echo "************ Check Group Membership ************";
-        purge_groups=$(gam info user $email| grep -A 100 "Groups:" |cut -d '<' -f2 |cut -d '>' -f1 |grep -v 'Groups:'|grep -B 100 'Licenses:'| grep -v 'Licenses:')|sort )
+        purge_groups=$($gam info user $email| grep -A 100 "Groups:" |cut -d '<' -f2 |cut -d '>' -f1 |grep -v 'Groups:'|grep -B 100 'Licenses:'| grep -v 'Licenses:')|sort )
         for i in $purge_groups
             do
                echo $i
@@ -72,7 +72,7 @@ do
    
      
      6) echo "************ Remove From All Groups ************";
-        purge_groups=$(gam info user $email| grep -A 100 "Groups:" |cut -d '<' -f2 |cut -d '>' -f1 |grep -v 'Groups:'|grep -B 100 'Licenses:'| grep -v 'Licenses:')
+        purge_groups=$($gam info user $email| grep -A 100 "Groups:" |cut -d '<' -f2 |cut -d '>' -f1 |grep -v 'Groups:'|grep -B 100 'Licenses:'| grep -v 'Licenses:')
            for i in $purge_groups
             do
                echo removing from $i
